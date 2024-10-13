2caf73efcf8c1cca5b050fe2be5b574ab3bc1e0e
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-13 10:22:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,6 +13,6 @@ variable "instance_type" {
 variable "key_pair_name" {
   description = "The name of the SSH key pair to use for the instance"
   type        = string
-  default     = "BomBay_Linux.pem"  # Replace with your actual key pair name
+  default     = "BomBay_Linux"  # Replace with your actual key pair name
 }
 
