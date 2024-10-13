ad572c01bebe32f50050a8328883574a5a46b27a
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-13 10:26:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "instance_type" {
   description = "The type of EC2 instance to create"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.medium"
 }
 
 variable "key_pair_name" {
